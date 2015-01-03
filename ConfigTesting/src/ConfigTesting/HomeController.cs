7e83dd8d3b525b079fc52287ac6c034a54b6bdcb
Aaron Powell <me@aaron-powell.com>
2015-01-03 15:29:47 +1100

    Config testing

---
+++ ConfigTesting/src/ConfigTesting/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.Framework.ConfigurationModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConfigTesting
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            var field = configuration.GetType()
+                .GetField("_sources", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+            var sources = field.GetValue(configuration) as IList<IConfigurationSource>;
+
+            var values = sources
+                .OfType<BaseConfigurationSource>()
+                .SelectMany(x => x.Data);
+
+            return View(values);
+        }
+    }
+}
---
+++ ConfigTesting/src/ConfigTesting/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.AspNet.Routing;
+
+namespace ConfigTesting
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            // Setup configuration sources.
+            Configuration = new Configuration()
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IConfiguration>(_ => Configuration);
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+        }
+    }
+}
